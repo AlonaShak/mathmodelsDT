e1130e446e15c49c00936e3e44319c0b023de80b
Alona <al_sh_94@ukr.net>
2022-11-16 09:48:31 +0200

    fixed 3

--- ex3-octave/plotData.m
+++ ex3-octave/plotData.m
@@ -8,11 +8,6 @@ figure; hold on;
 
 % ====================== Ваш код ======================
 
-% Знаходження індексів позитивних і негативних прикладів
-pos = find(y==1); neg = find(y == 0);
-% Побудова діаграми розсіювання
-plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
-plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 
 
